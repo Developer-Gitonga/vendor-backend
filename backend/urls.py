464d32d8e09df85dfede32aa015992cb252f4d44
Developer-Gitonga <aizensalim52@@gmail.com>
2023-06-22 14:07:29 +0300

    feat:service

--- backend/urls.py
+++ backend/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import *
 
+app_name = 'backend'
+
 urlpatterns = [
     path('tasks/',TaskViewSet.as_view(),name = "task"),
     path('catalogue/',CatalogueViewSet.as_view(), name = "catalogue"),
-    path('product/', ProductViewSet.as_view(), name= "product")
-    # path('tasks/<int:pk>/', TaskViewSet.as_view(),name = "task-detail")
+    path('product/', ProductViewSet.as_view(), name= "product"),
+    path('tasks/<int:pk>/', TaskViewSet.as_view(),name = "task-detail"),
+    path('catalogue/<int:pk>/', CatalogueViewSet.as_view(),name = "task-detail"),
+    path('product/<int:pk>/', ProductViewSet.as_view(),name = "task-detail")
+
 ]
